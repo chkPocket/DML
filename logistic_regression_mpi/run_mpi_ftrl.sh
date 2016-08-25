c8020c16dab10471f77a80cead4d36c4208ccc71
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-08-25 21:49:50 +0800

    update run_mpi_ftrl.sh

--- logistic_regression_mpi/run_mpi_ftrl.sh
+++ logistic_regression_mpi/run_mpi_ftrl.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
-#tt=`date`
-#mkdir backup/"$tt"
-#mv train backup/"$tt"
-#mv *.log backup/"$tt"
-#mv core backup/"$tt"
-#make
-#rm log/*
+process_number=4
+Ip=("10.101.2.89" "10.101.2.90")
+for ip in ${Ip[@]}
+do
+    ssh worker@$ip rm /home/worker/xiaoshu/DML/logistic_regression_mpi/train
+done
 scp train worker@10.101.2.89:/home/worker/xiaoshu/DML/logistic_regression_mpi/.
 scp train worker@10.101.2.90:/home/worker/xiaoshu/DML/logistic_regression_mpi/.
-mpirun -f ../hosts -np 6 ./train ftrl ./data/agaricus.txt.train ./data/agaricus.txt.test
+mpirun -f ../hosts -np $process_number ./train ftrl ./data/agaricus.txt.train ./data/agaricus.txt.test
